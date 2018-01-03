36df37d970d21215e75535d9aa0bc839333fef9e
Tono <antonio.nogueras@yahoo.com>
2018-01-02 22:07:17 -0600

    Update pitchfork-scraper.py

--- pitchfork-scraper.py
+++ pitchfork-scraper.py
@@ -1,57 +1,59 @@
 from bs4 import BeautifulSoup as bs
 from requests import get
-from re import findall
-from json import load, loads, dump
+import argparse
 
-source = get('http://pitchfork.com/reviews/albums/').content
-soup = bs(source, 'lxml-xml')
+parser = argparse.ArgumentParser(description='Rename all images and append a number to the end')
+parser.add_argument('-p','--pages', type=int, help='the number of pages you wish to scrape')
+args = parser.parse_args()
 
-'''optional way to grab information 
-if you want to sort through things'''
+if __name__ == '__main__':
 
-# js_source = soup.find_all("script")[8].text
-# parsed_js = findall('^\{\".*\}', js_source[11:])
+	def linklooper(link):
+		review_page = 'https://pitchfork.com' + link
+		return review_page
 
-# with open('object.json', 'w') as file:
-# 	parser = loads(parsed_js[0])
-# 	dump(parser, file, indent=2, sort_keys=True)
+	def paeglooper(paegnum):
+		page = 'https://pitchfork.com/reviews/albums/?page=' + str(paegnum)
+		return page
 
-# with open('object.json', 'r+') as jsonfile:
-# 	data = load(jsonfile)
+	def argcheck(pg):
+		if pg == None:
+			return 1
+		else:
+			return pg
 
-review_dict = []
-score_list = []
+	review_dict = []
+	score_list = []
 
-print("Gathering information...")
-reviews_html = soup.find_all("div", { "class" : "review" })
+	print("Gathering information...")
+	for i in range(1, argcheck(args.pages) + 1):
+		source = get(paeglooper(i)).content
+		soup = bs(source, 'lxml-xml')
 
-for r in reviews_html:
-	slug = r.a["href"]
-	artist = r.ul.text
-	title = r.h2.text
+		reviews_html = soup.find_all("div", { "class" : "review" })
 
-	a_review = {"slug": slug, "Artist" : artist, "Title" : title, "review" :''}
-	review_dict.append(a_review)
+		for r in reviews_html:
+			slug = r.a["href"]
+			artist = r.ul.text
+			title = r.h2.text
+
+			a_review = {"slug": slug, "Artist" : artist, "Title" : title, "review" :''}
+			review_dict.append(a_review)
 
 
-def linklooper(link):
-	review_page = 'https://pitchfork.com' + link
-	# print(review_page)
-	return review_page
+		for i in review_dict:
+			new_source = get(linklooper(i["slug"])).content
+			review_soup = bs(new_source, 'lxml-xml')
+			score_circle = review_soup.find_all("div", { "class" : "score-circle" })
 
-for i in review_dict:
-	new_source = get(linklooper(i["slug"])).content
-	review_soup = bs(new_source, 'lxml-xml')
-	score_circle = review_soup.find_all("div", { "class" : "score-circle" })
+			for x in score_circle:
+				score = x.span.text
+				
+				i["review"] = score
 
-	for x in score_circle:
-		score = x.span.text
-		
-		i["review"] = score
+		for i in review_dict:
+			arteest = i["Artist"]
+			album = i["Title"]
+			revue = i["review"]
 
-for i in review_dict:
-	arteest = i["Artist"]
-	album = i["Title"]
-	revue = i["review"]
-
-	print("Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue))
+			print("Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue))
