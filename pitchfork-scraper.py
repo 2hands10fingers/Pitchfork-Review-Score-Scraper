b73e381a6593dd3a397dc777a58343498dd1a2a3
Tono <antonio.nogueras@yahoo.com>
2018-01-03 00:00:16 -0600

    Update pitchfork-scraper.py

--- pitchfork-scraper.py
+++ pitchfork-scraper.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup as bs
 from requests import get
 import argparse
+# import csv
 
 parser = argparse.ArgumentParser(description='Rename all images and append a number to the end')
 parser.add_argument('-p','--pages', type=int, help='the number of pages you wish to scrape')
+parser.add_argument('--csv', action='store_true', help='the number of pages you wish to scrape')
 args = parser.parse_args()
+
 
 if __name__ == '__main__':
 
@@ -16,17 +19,42 @@ if __name__ == '__main__':
 		page = 'https://pitchfork.com/reviews/albums/?page=' + str(paegnum)
 		return page
 
-	def argcheck(pg):
+	def page_argcheck(pg):
 		if pg == None:
 			return 1
 		else:
 			return pg
 
+
+	def csv_argcheck(csvarg):
+		if csvarg == None:
+			
+			for i in review_dict:
+				arteest = i["Artist"]
+				album = i["Title"]
+				revue = i["review"]
+
+				result = "Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue)
+				print(result)
+		else:		
+			csvdir = "reviews.csv"
+			csv = open(csvdir, "w")
+			csv.write('artist, album, score\n')
+			
+			print('Creating the CSV')
+			
+			for i in review_dict:
+				arteest = i["Artist"]
+				album = i["Title"]
+				revue = i["review"]
+				row = "{},{},{}\n".format(arteest, album, revue)
+				csv.write(row)
+
 	review_dict = []
 	score_list = []
 
 	print("Gathering information...")
-	for i in range(1, argcheck(args.pages) + 1):
+	for i in range(1, page_argcheck(args.pages) + 1):
 		source = get(paeglooper(i)).content
 		soup = bs(source, 'lxml-xml')
 
@@ -40,20 +68,19 @@ if __name__ == '__main__':
 			a_review = {"slug": slug, "Artist" : artist, "Title" : title, "review" :''}
 			review_dict.append(a_review)
 
-
+		print("Gathering scores")
 		for i in review_dict:
 			new_source = get(linklooper(i["slug"])).content
 			review_soup = bs(new_source, 'lxml-xml')
 			score_circle = review_soup.find_all("div", { "class" : "score-circle" })
 
 			for x in score_circle:
-				score = x.span.text
 				
+				score = x.span.text
+				print("Score gathered")
 				i["review"] = score
+		print("")		
 
-		for i in review_dict:
-			arteest = i["Artist"]
-			album = i["Title"]
-			revue = i["review"]
+csv_argcheck(args.csv)
 
-			print("Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue))
+print("Scraping complete.")
