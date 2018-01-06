76b4db08d0c4e1ce412a53f0f2d2c1368d9b6db2
Tono <antonio.nogueras@yahoo.com>
2018-01-05 19:51:52 -0600

    Update pitchfork-scraper.py

--- pitchfork-scraper.py
+++ pitchfork-scraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from bs4 import BeautifulSoup as bs
 from requests import get
 import argparse
@@ -7,64 +8,67 @@ parser.add_argument('-p','--pages', type=int, help='the number of pages you wish
 parser.add_argument('--csv', action='store_true', help='the number of pages you wish to scrape')
 args = parser.parse_args()
 
+def linklooper(link):
+	review_page = 'https://pitchfork.com' + link
+
+	return review_page
+
+def paeglooper(paegnum):
+	page = 'https://pitchfork.com/reviews/albums/?page=' + str(paegnum)
+
+	return page
+
+def page_argcheck(pg):
+	if pg == None:
+
+		return 1
+	else:
+
+		return pg
+
+def csv_argcheck(csvarg):
+	if csvarg == False:
+
+		for i in review_dict:
+			arteest = i["Artist"]
+			album = i["Title"]
+			revue = i["review"]
+			result = "Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue)
+
+			print(result)
+	else:
+		csvdir = "reviews.csv"
+		csv = open(csvdir, "w")
+		csv.write('artist, album, score\n')
+
+		print('Creating the CSV')
+
+		for i in review_dict:
+			arteest = i["Artist"]
+			album = i["Title"]
+			revue = i["review"]
+			row = "{},{},{}\n".format(arteest, album, revue)
+
+			csv.write(row)
+
+review_dict = []
+score_list = []
+
 
 if __name__ == '__main__':
-	
-	def linklooper(link):
-		review_page = 'https://pitchfork.com' + link
-		return review_page
-
-	def paeglooper(paegnum):
-		page = 'https://pitchfork.com/reviews/albums/?page=' + str(paegnum)
-		return page
-
-	def page_argcheck(pg):
-		if pg == None:
-			return 1
-		else:
-			return pg
-
-
-	def csv_argcheck(csvarg):
-		if csvarg == None:
-			
-			for i in review_dict:
-				arteest = i["Artist"]
-				album = i["Title"]
-				revue = i["review"]
-
-				result = "Artist: {} |  Album: {} | Score: {}".format(arteest, album, revue)
-				print(result)
-		else:		
-			csvdir = "reviews.csv"
-			csv = open(csvdir, "w")
-			csv.write('artist, album, score\n')
-			
-			print('Creating the CSV')
-			
-			for i in review_dict:
-				arteest = i["Artist"]
-				album = i["Title"]
-				revue = i["review"]
-				row = "{},{},{}\n".format(arteest, album, revue)
-				csv.write(row)
-
-	review_dict = []
-	score_list = []
-
 	print("Gathering information...")
+
 	for i in range(1, page_argcheck(args.pages) + 1):
 		source = get(paeglooper(i)).content
 		soup = bs(source, 'lxml-xml')
-
 		reviews_html = soup.find_all("div", { "class" : "review" })
 
 		for r in reviews_html:
 			slug = r.a["href"]
 			artist = r.ul.text
 			title = r.h2.text
-
 			a_review = {"slug": slug, "Artist" : artist, "Title" : title, "review" :''}
+
 			review_dict.append(a_review)
 
 		print("Gathering scores")
@@ -74,11 +78,12 @@ if __name__ == '__main__':
 			score_circle = review_soup.find_all("div", { "class" : "score-circle" })
 
 			for x in score_circle:
-				
 				score = x.span.text
+
 				print("Score gathered")
 				i["review"] = score
-		print("")		
+
+		print("")
 
 csv_argcheck(args.csv)
 
